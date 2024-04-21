1d2e42809eae651288e2255d98de88e9336dc186
yuyutti <yuyutti@yuyutti.com>
2024-04-21 11:40:18 +0900

    commits

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
 const express = require('express');
-const app = express();
+const { connect } = require('puppeteer-real-browser');
+const Queue = require('queue');
 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let browser, page;
+let browser;
+const queue = new Queue({ concurrency: 1, autostart: true });
 
-app.post('/api/user', async (req, res) => {
+app.post('/api/user', (req, res) => {
     console.log(`Request received: ${req.body.url}`);
     const url = req.body.url;
     if (!url) return res.status(404).send('URL not found');
 
-    const data = await main(url);
-    console.log(data);
-    if (!data) return res.status(500).json({ error: 'Failed to fetch data' });
-
-    res.json(data);
+    queue.push(async cb => {
+        const data = await processUrl(url);
+        if (!data) {
+            res.status(500).json({ error: 'Failed to fetch data' });
+        } else {
+            res.json(data);
+        }
+        cb();
+    });
 });
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+async function processUrl(url) {
+    try {
+        const page = await browser.newPage();
+        console.log(url);
+        await page.goto(url);
+        await sleep(7000);
+        const html = await page.content();
+        const scriptRegex = /const profile = (\{[\s\S]*?"powerRank":\s*(\{[\s\S]*?\})[\s\S]*?\});/m;
+        const match = scriptRegex.exec(html);
+        await page.close();
+        return match && match[1] ? JSON.parse(match[1]) : null;
+    } catch (error) {
+        console.error('Error in fetching or processing page:', error);
+        return null;
+    }
 }
 
 async function setupBrowser() {
-    const { connect } = await import('puppeteer-real-browser');
     const connection = await connect({ headless: false, turnstile: true });
     browser = connection.browser;
 }
 
-async function main(url) {
-    try {
-        page = await browser.newPage();
-        console.log(url)
-        await page.goto(url);
-        await sleep(7000);
-
-        const html = await page.content();
-        const scriptRegex = /const profile = (\{[\s\S]*?"powerRank":\s*(\{[\s\S]*?\})[\s\S]*?\});/m;
-        const match = scriptRegex.exec(html);
-        
-        if (match && match[1]) {
-            return JSON.parse(match[1]);
-        } else {
-            console.error('Profile data not found');
-            return null;
-        }
-    } catch (error) {
-        console.error('Error in fetching or processing page:', error);
-        return null;
-    } finally {
-        await page.close();
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 (async () => {
