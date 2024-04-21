f502e0f3aea908ec48f04e8bc9ace172e005f00a
yuyutti <yuyutti@yuyutti.com>
2024-04-21 12:18:44 +0900

    刈り込みッと

--- index.js
+++ index.js
@@ -1,50 +1,71 @@
 const express = require('express');
-const { connect } = require('puppeteer-real-browser');
-const Queue = require('queue');
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let browser;
-const queue = new Queue({ concurrency: 1, autostart: true });
+let BROWSER;
+const taskQueue = [];
+let isProcessing = false;
+
+async function processQueue() {
+    if (isProcessing) return;
+    isProcessing = true;
+
+    while (taskQueue.length > 0) {
+        const task = taskQueue.shift();
+        try {
+            await task();
+        } catch (error) {
+            console.error('Error processing task:', error);
+        }
+    }
+
+    isProcessing = false;
+}
 
 app.post('/api/user', (req, res) => {
-    console.log(`Request received: ${req.body.url}`);
     const url = req.body.url;
     if (!url) return res.status(404).send('URL not found');
 
-    queue.push(async cb => {
+    taskQueue.push(async () => {
+        console.log('Processing URL:', url);
         const data = await processUrl(url);
         if (!data) {
             res.status(500).json({ error: 'Failed to fetch data' });
         } else {
             res.json(data);
         }
-        cb();
     });
+
+    processQueue();
 });
 
 async function processUrl(url) {
     try {
-        const page = await browser.newPage();
-        console.log(url);
-        await page.goto(url);
+        const { page, browser, setTarget } = BROWSER;
+        setTarget({ status: false });
+        const page2 = await browser.newPage();
+        setTarget({ status: true });
+        await page2.goto(url, { waitUntil: 'domcontentloaded' });
         await sleep(7000);
-        const html = await page.content();
+        const html = await page2.content();
+        await page2.close();
         const scriptRegex = /const profile = (\{[\s\S]*?"powerRank":\s*(\{[\s\S]*?\})[\s\S]*?\});/m;
         const match = scriptRegex.exec(html);
-        await page.close();
         return match && match[1] ? JSON.parse(match[1]) : null;
     } catch (error) {
         console.error('Error in fetching or processing page:', error);
-        return null;
+        throw error;
     }
 }
 
 async function setupBrowser() {
-    const connection = await connect({ headless: false, turnstile: true });
-    browser = connection.browser;
+    const { connect } = await import('puppeteer-real-browser');
+    const connection = await connect({ tf: true, turnstile: true, fingerprint: true, headless: 'auto', });
+    const { page, browser, setTarget} = connection;
+    await page.goto('https://google.com');
+    BROWSER = { page, browser, setTarget };
+    
 }
 
 function sleep(ms) {
